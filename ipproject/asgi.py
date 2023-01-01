fb1c2684577defe8a482c2f3a23529ee95f1065b
JustGithubProject <Kropivadanil@gmail.com>
2023-01-01 22:02:09 +0300

    rename

--- ipproject/asgi.py
+++ ipproject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for wikipediasearch project.
+ASGI config for ipproject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wikipediasearch.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ipproject.settings')
 
 application = get_asgi_application()
--- ipproject/wsgi.py
+++ ipproject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for wikipediasearch project.
+WSGI config for ipproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wikipediasearch.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ipproject.settings')
 
 application = get_wsgi_application()
